30afc6f82236d4e8525e344b9bf75f8ebd2eb409
kendid <81610710+Daphyk@users.noreply.github.com>
2022-02-19 23:54:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 3.7.4
 da39a3ee5e6b4b0d3255bfef
 js.script 
+/**
+* Template Name: iPortfolio - v3.6.0
+* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(funct
